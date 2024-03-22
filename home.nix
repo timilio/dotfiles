b7e757899c2cef3fb757edbb23c16a94910ad877
timilio <42062607+timilio@users.noreply.github.com>
2024-03-22 16:44:06 +0100

    Add java (pls clean up later)

--- home.nix
+++ home.nix
@@ -27,6 +27,7 @@
     du-dust
     ghc
     typst
+    temurin-bin # java
 
     (pkgs.nerdfonts.override {fonts = ["NerdFontsSymbolsOnly"];})
     pkgs.jetbrains-mono
@@ -39,6 +40,7 @@
     ruff-lsp
     taplo
     typst-lsp
+    jdt-language-server # java
 
     alejandra
     biome
@@ -236,11 +238,12 @@
       cmp-pandoc-references
       cmp-fish
 
-      (nvim-treesitter.withPlugins (p: [p.bash p.c p.comment p.cpp p.css p.csv p.doxygen p.elixir p.gitignore p.fennel p.fish p.haskell p.html p.javascript p.latex p.lua p.markdown p.markdown_inline p.nix p.ocaml p.ocaml_interface p.printf p.python p.rust p.sql p.toml p.typst p.vimdoc p.zig]))
+      (nvim-treesitter.withPlugins (p: [p.bash p.c p.comment p.cpp p.css p.csv p.doxygen p.elixir p.gitignore p.fennel p.fish p.haskell p.html p.java p.javascript p.latex p.lua p.markdown p.markdown_inline p.nix p.ocaml p.ocaml_interface p.printf p.python p.rust p.sql p.toml p.typst p.vimdoc p.zig]))
       nvim-treesitter-textobjects
       rainbow-delimiters-nvim
       typst-vim
       haskell-tools-nvim
+      nvim-jdtls # java
 
       crates-nvim
       nabla-nvim
@@ -347,6 +350,7 @@
   home.file = {
     ".clang-format".source = ./clang-format;
     ".cargo/config.toml".source = ./config/cargo/config.toml;
+    ".local/share/jdtls/config_linux/config.ini".source = builtins.toPath "${pkgs.jdt-language-server}/share/java/jdtls/config_linux/config.ini";
   };
 
   xdg = {
@@ -368,6 +372,58 @@
           done
         '';
       };
+      "nvim/ftplugin/java.lua".text = "
+-- See `:help vim.lsp.start_client` for an overview of the supported `config` options.
+local config = {
+  -- The command that starts the language server
+  -- See: https://github.com/eclipse/eclipse.jdt.ls#running-from-the-command-line
+  cmd = {
+    '${pkgs.temurin-bin}/bin/java',
+
+    '-Declipse.application=org.eclipse.jdt.ls.core.id1',
+    '-Dosgi.bundles.defaultStartLevel=4',
+    '-Declipse.product=org.eclipse.jdt.ls.core.product',
+    '-Dlog.protocol=true',
+    '-Dlog.level=ALL',
+    '-Xmx1g',
+    '--add-modules=ALL-SYSTEM',
+    '--add-opens', 'java.base/java.util=ALL-UNNAMED',
+    '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
+
+    '-jar', vim.fn.glob('${pkgs.jdt-language-server}/share/java/jdtls/plugins/org.eclipse.equinox.launcher_*.jar'),
+
+    '-configuration', '/home/${username}/.local/share/jdtls/config_linux',
+
+    -- See `data directory configuration` section in the README
+    '-data', '/home/${username}/.local/share/jdtls/data/' .. vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
+  },
+
+  -- This is the default if not provided, you can remove it. Or adjust as needed.
+  -- One dedicated LSP server & client will be started per unique root_dir
+  root_dir = require('jdtls.setup').find_root({'.git', 'mvnw', 'gradlew'}),
+
+  -- Here you can configure eclipse.jdt.ls specific settings
+  -- See https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
+  -- for a list of options
+  settings = {
+    java = {
+    }
+  },
+
+  -- Language server `initializationOptions`
+  -- You need to extend the `bundles` with paths to jar files
+  -- if you want to use additional eclipse.jdt.ls plugins.
+  --
+  -- See https://github.com/mfussenegger/nvim-jdtls#java-debug-installation
+  --
+  -- If you don't plan on using the debugger or other eclipse.jdt.ls plugins you can remove this
+  init_options = {
+    bundles = {}
+  },
+}
+-- This starts a new client & server,
+-- or attaches to an existing client & server depending on the `root_dir`.
+require('jdtls').start_or_attach(config)";
       "rustfmt".source = ./config/rustfmt;
       "zk".source = ./config/zk;
     };
