bf81df4e0f350a271657e23f3b41220e4130c69e
timilio <42062607+timilio@users.noreply.github.com>
2024-06-14 16:33:31 +0200

    Fix

--- home.nix
+++ home.nix
@@ -14,6 +14,8 @@
     ./neovim.nix
   ];
 
+  fonts.fontconfig.enable = true;
+
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [
@@ -66,6 +68,8 @@
     extraConfig = ''
       modify_font cell_height 1px
       modify_font baseline -2
+
+      symbol_map U+e000-U+e00a,U+ea60-U+ebeb,U+e0a0-U+e0c8,U+e0ca,U+e0cc-U+e0d4,U+e200-U+e2a9,U+e300-U+e3e3,U+e5fa-U+e6b1,U+e700-U+e7c5,U+f000-U+f2e0,U+f300-U+f372,U+f400-U+f532,U+f0001-U+f1af0 Symbols Nerd Font Mono
     '';
     theme = "Gruvbox Dark";
     settings = {
